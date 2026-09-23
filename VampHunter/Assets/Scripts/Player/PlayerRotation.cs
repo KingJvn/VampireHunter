9b46bc4938fd6257c9c823f9134bac1c97fdc523
Jobe <jvernick93@gmail.com>
2026-09-23 15:32:05 -0400

    Added rotation smoothing
    Can adjust the speed at which the player rotates in the rotator script

--- VampHunter/Assets/Scripts/Player/PlayerRotation.cs
+++ VampHunter/Assets/Scripts/Player/PlayerRotation.cs
@@ -4,10 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerRotation : Rotator
 {
     public InputActionReference look;
+    private Vector3 mouseWorldPosition;
+    private void Start()
+    {
+        mouseWorldPosition = transform.position + transform.up; //initialize start position
+    }
+    private void Update()
+    {
+        RotateToward(mouseWorldPosition); //rotates player every frame to independently from the mouse movement. Stops player from pausing when not finished rotating
+    }
     private void OnLook(InputAction.CallbackContext context)
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
-        LookAt(mousePosition);
+        Vector2 screenPos = context.ReadValue<Vector2>();
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(screenPos);
     }
 
     private void OnEnable()
--- VampHunter/Assets/Scripts/Player/Rotator.cs
+++ VampHunter/Assets/Scripts/Player/Rotator.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Rotator : MonoBehaviour
 {
-    protected void LookAt(Vector3 target)
+    private float rotationSpeed = 440f;
+    protected void RotateToward(Vector3 target)
     {
-        //calculate angle between transform and target
-        float lookAngle = AngleBetweenTwoPoints(transform.position, target) + 90;
+        float targetAngle = AngleBetweenTwoPoints(transform.position, target) + 90f; //calculate final target angle
 
-        //assign the target rotation on the z axis
-        transform.eulerAngles = new Vector3(0, 0, lookAngle);
+        float currentAngle = transform.eulerAngles.z; // get current Z rotation
+
+        float step = rotationSpeed * Time.deltaTime;
+        float nextAngle = Mathf.MoveTowardsAngle(currentAngle, targetAngle, step); //smoothly move current angle to target angle
+
+        transform.eulerAngles = new Vector3(0, 0, nextAngle);
     }
 
     private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
     {
         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-        
     }
 }
