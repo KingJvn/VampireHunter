8e1e62b63492bdcc3ae016de79a604ffe4f8f32f
Jobe <jvernick93@gmail.com>
2026-09-23 15:58:50 -0400

    Added Sprint
    Sprint logic stored in player movement script

--- VampHunter/Assets/Scripts/Player/PlayerMovement.cs
+++ VampHunter/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,15 +3,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float sprintSpeed = 3.5f;
+    [SerializeField] private float currentSpeed = 0;
     private Rigidbody2D rb;
     private Vector2 moveDirection;
 
     public InputActionReference move;
+    public InputActionReference sprint;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        currentSpeed = walkSpeed;
     }
     private void Update()
     {
@@ -20,15 +27,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+        rb.linearVelocity = new Vector2(moveDirection.x * currentSpeed, moveDirection.y * currentSpeed);
     }
 
+    private void OnSprint(InputAction.CallbackContext context)
+    {
+        if(context.ReadValueAsButton())
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+        
+    }
     private void OnEnable()
     {
         move.action.Enable();
+        sprint.action.performed += OnSprint;
+        sprint.action.canceled += OnSprint;
     }
     private void OnDisable()
     {
         move.action.Disable();
+        sprint.action.performed -= OnSprint;
+        sprint.action.canceled -= OnSprint;
     }
 }
