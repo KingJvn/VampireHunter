8a8eb57836a01978db947622936d8a7e1d764709
Jobe <jvernick93@gmail.com>
2026-09-23 12:51:10 -0400

    Working on melee system

--- VampHunter/Assets/Scripts/Player/PlayerController.cs
+++ VampHunter/Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Weapon equippedWeapon;
+
+    private int comboStep = 0;
+    private float lastAttackTime = 0f;
+    [SerializeField] private float comboResetTime = 1f;
+
+    private Animator animator;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Time.time - lastAttackTime > comboResetTime && comboStep > 0)
+        {
+            ResetCombo();
+        }
+    }
+
+    public void OnAttack(InputValue value)
+    {
+        if (value.isPressed || equippedWeapon == null) return;
+
+        if(Time.time >= lastAttackTime + equippedWeapon.AttackCooldown)
+        {
+            if(equippedWeapon is MeleeWeapon)
+            {
+                comboStep++;
+                if (comboStep > 2) comboStep = 1;
+            }
+            else
+            {
+                comboStep = 1;
+            }
+
+            lastAttackTime = Time.time;
+
+            animator.SetInteger("ComboStep", comboStep);
+            animator.SetTrigger("Attack");
+
+            equippedWeapon.Attack();
+        }
+    }
+
+    public void ResetCombo()
+    {
+        comboStep = 0;
+        animator.SetInteger("ComboStep", 0);
     }
 }
--- VampHunter/Assets/Scripts/Player/Weapon.cs
+++ VampHunter/Assets/Scripts/Player/Weapon.cs
@@ -6,5 +6,6 @@ public abstract class Weapon : MonoBehaviour
     [SerializeField] protected int damage = 10;
     [SerializeField] protected float attackCooldown = 0.5f;
 
+    public float AttackCooldown => attackCooldown;
     public abstract void Attack();
 }
